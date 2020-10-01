c1baa7c904c85fb7b7562d9e26fe7d8d07c49d19
Saso Hribernik <saso.hribernik@hribis.com>
2020-10-01 10:12:40 +0100

    fix: slideIndexBox calculation

--- js/main.js
+++ js/main.js
@@ -1,30 +1,62 @@
 (function () {
   function scrollHorizontally(e) {
     setScroll(e);
-    calcSlideIndexBoxOffset();
+    calcSlideIndexBoxOffset('slide3');
   }
 
   function setScroll(e) {
     e = window.event || e;
-    var delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    const delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
     document.documentElement.scrollLeft -= delta * 60;
     document.body.scrollLeft -= delta * 60;
-    // e.preventDefault();
   }
 
-  function calcSlideIndexBoxOffset() {
-    const scrollY = window.scrollY;
+  // function calcSlideIndexBoxOffset() {
+  //   const scrollY = window.scrollY;
 
-    const slide = document.getElementsByClassName('slide3')[0];
+  //   const slide = document.getElementsByClassName('slide3')[0];
+  //   const box = slide.getElementsByClassName('slide-index-box')[0];
+
+  //   const slideOffset = slide.offsetLeft;
+  //   const boxOffset = box.offsetLeft;
+  //   const margin = boxOffset - slideOffset;
+  //   console.log('slide: ', slideOffset, 'box: ', boxOffset);
+
+  //   if (scrollY > slideOffset && margin > 0) {
+  //     const newMargin = margin - (scrollY - slideOffset);
+  //     slide.style.marginLeft = `${newMargin}px`;
+  //   }
+  // }
+
+  function getSlidePosition(slideClassName) {
+    const slide = document.getElementsByClassName(slideClassName)[0];
+    const slideStart = slide.offsetLeft;
+    const slideEnd = slideStart + slide.offsetWidth;
+
+    return { slide, slideStart, slideEnd };
+  }
+
+  function calcSlideIndexBoxOffset(slideClassName) {
+    const margin = 300;
+    const scrollX = window.scrollX;
+    const { slide, slideStart, slideEnd } = getSlidePosition(slideClassName);
     const box = slide.getElementsByClassName('slide-index-box')[0];
+    const boxOffset = box.offsetLeft;
 
-    const slideOffset = slide.offsetLeft;
-    const boxOffset   = box.offsetLeft;
-    const margin  = boxOffset - slideOffset;
+    if (
+      scrollX > slideStart + boxOffset &&
+      scrollX < slideEnd &&
+      boxOffset > 0
+    ) {
+      box.style.marginLeft = `0px`;
+    }
 
-    if (scrollY > slideOffset && margin > 0) {
-      const newMargin = margin - (scrollY - slideOffset);
-        slide.style.marginLeft = `${newMargin}px`;
+    if (
+      scrollX > slideStart &&
+      boxOffset === 0 &&
+      scrollX < slideStart + margin
+    ) {
+      box.style.marginLeft = `${margin}px`;
     }
   }
 
@@ -37,92 +69,91 @@
     // IE 6/7/8
     window.attachEvent('onmousewheel', scrollHorizontally);
   }
-
 })();
 
-let Slide3Documents;
-let prevRatio = 0.0;
-let scaleRatio = 'scale(ratio);';
-let decreasingColor = 'rgba(190, 40, 40, ratio)';
+// let Slide3Documents;
+// let prevRatio = 0.0;
+// let scaleRatio = 'scale(ratio);';
+// let decreasingColor = 'rgba(190, 40, 40, ratio)';
 
-const articleOne = document.querySelector('#article-one');
-const articleTwo = document.querySelector('#article-two');
-const articleThree = document.querySelector('#article-three');
+// const articleOne = document.querySelector('#article-one');
+// const articleTwo = document.querySelector('#article-two');
+// const articleThree = document.querySelector('#article-three');
 
-// Set things up
-window.addEventListener(
-  'load',
-  event => {
-    Slide3Documents = document.querySelector('#slide3-documents');
-    createObserver();
-  },
-  false
-);
+// // Set things up
+// window.addEventListener(
+//   'load',
+//   event => {
+//     Slide3Documents = document.querySelector('#slide3-documents');
+//     createObserver();
+//   },
+//   false
+// );
 
-function buildThresholdList() {
-  let thresholds = [];
-  let numSteps = 100;
+// function buildThresholdList() {
+//   let thresholds = [];
+//   let numSteps = 100;
 
-  for (let i = 1.0; i <= numSteps; i++) {
-    let ratio = i / numSteps;
-    thresholds.push(ratio);
-  }
+//   for (let i = 1.0; i <= numSteps; i++) {
+//     let ratio = i / numSteps;
+//     thresholds.push(ratio);
+//   }
 
-  thresholds.push(0);
-  console.log(thresholds);
-  return thresholds;
-}
+//   thresholds.push(0);
+//   console.log(thresholds);
+//   return thresholds;
+// }
 
-function createObserver() {
-  let observer;
+// function createObserver() {
+//   let observer;
 
-  let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: buildThresholdList(),
-  };
+//   let options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: buildThresholdList(),
+//   };
 
-  observer = new IntersectionObserver(handleIntersect, options);
-  observer.observe(Slide3Documents);
-}
+//   observer = new IntersectionObserver(handleIntersect, options);
+//   observer.observe(Slide3Documents);
+// }
 
-function round5(x) {
-  return Math.ceil(x / 5) * 5;
-}
+// function round5(x) {
+//   return Math.ceil(x / 5) * 5;
+// }
 
-function handleIntersect(entries, observer) {
-  entries.forEach(entry => {
-    const position = Math.ceil(entry.intersectionRatio * 100);
-    console.log(position);
+// function handleIntersect(entries, observer) {
+//   entries.forEach(entry => {
+//     const position = Math.ceil(entry.intersectionRatio * 100);
+//     // console.log(position);
 
-    if (position < 15) {
-      articleOne.style.transform = `scale(0.20)`;
-    } else if (position < 20) {
-      articleOne.style.transform = `scale(0.35)`;
-    } else if (position < 25) {
-      articleOne.style.transform = `scale(0.50)`;
-    } else if (position < 30) {
-      articleOne.style.transform = `scale(0.75)`;
-      articleTwo.style.transform = `scale(0.20)`;
-    } else if (position < 40) {
-      articleOne.style.transform = `scale(1)`;
-      articleTwo.style.transform = `scale(0.35)`;
-    } else if (position < 50) {
-      articleTwo.style.transform = `scale(0.50)`;
-    } else if (position < 55) {
-      articleTwo.style.transform = `scale(0.75)`;
-    } else if (position < 60) {
-      articleTwo.style.transform = `scale(0.1)`;
-    } else if (position < 65) {
-    } else if (position < 70) {
-    } else if (position < 75) {
-    } else if (position < 80) {
-    } else if (position < 85) {
-    } else if (position < 90) {
-    } else if (position < 95) {
-    } else if (position < 100) {
-    }
+//     if (position < 15) {
+//       articleOne.style.transform = `scale(0.20)`;
+//     } else if (position < 20) {
+//       articleOne.style.transform = `scale(0.35)`;
+//     } else if (position < 25) {
+//       articleOne.style.transform = `scale(0.50)`;
+//     } else if (position < 30) {
+//       articleOne.style.transform = `scale(0.75)`;
+//       articleTwo.style.transform = `scale(0.20)`;
+//     } else if (position < 40) {
+//       articleOne.style.transform = `scale(1)`;
+//       articleTwo.style.transform = `scale(0.35)`;
+//     } else if (position < 50) {
+//       articleTwo.style.transform = `scale(0.50)`;
+//     } else if (position < 55) {
+//       articleTwo.style.transform = `scale(0.75)`;
+//     } else if (position < 60) {
+//       articleTwo.style.transform = `scale(0.1)`;
+//     } else if (position < 65) {
+//     } else if (position < 70) {
+//     } else if (position < 75) {
+//     } else if (position < 80) {
+//     } else if (position < 85) {
+//     } else if (position < 90) {
+//     } else if (position < 95) {
+//     } else if (position < 100) {
+//     }
 
-    prevRatio = entry.intersectionRatio;
-  });
-}
+//     prevRatio = entry.intersectionRatio;
+//   });
+// }
